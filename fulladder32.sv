3c65a00e5f6b60e1bdf5a22d65e04356e3bbb3a6
StuDenTMNF <87866777m@gmail.com>
2025-08-08 07:52:51 +0300

    Update fulladder32.sv

--- fulladder32.sv
+++ fulladder32.sv
@@ -1,29 +1,38 @@
-// Файл: full_adder_32bit.sv
+
 
 // --- Базовые гейты ---
-module Andd(input logic A, B, output logic C);
+module Andd
+  (input logic A, B, output logic C);
   assign C = A & B;
 endmodule
 
-module Orr(input logic A, B, output logic C);
+module Orr
+  (input logic A, B, output logic C);
   assign C = A | B;
 endmodule
 
-module Xorr(input logic A, B, output logic C);
+module Xorr
+  (input logic A, B, output logic C);
   assign C = A ^ B;
 endmodule
 
-// --- Однобитный полный сумматор ---
 module fulladder(
-  input  logic a_i, b_i, carry_i,
-  output logic sum_o, carry_o
-);
-  logic q1;
-  // Более эффективная реализация
-  assign q1 = a_i ^ b_i;
-  assign sum_o = q1 ^ carry_i;
-  assign carry_o = (a_i & b_i) | (q1 & carry_i);
+input a_i,
+input b_i, 
+input carry_i,
+output sum_o,
+output carry_o); 
+
+wire q, w, e, r, t,y;
+Xorr moduleXOR1( .A(a_i),     .B(b_i),     .C(q)       );
+Xorr moduleXOR2( .A(q),       .B(carry_i), .C(sum_o)   );
+Andd moduleAND1( .A(a_i),     .B(carry_i), .C(w)       );
+Andd moduleAND2( .A(a_i),     .B(b_i),     .C(e)       );
+Andd moduleAND3( .A(carry_i), .B(b_i),     .C(r)       );
+Orr  moduleOR1 ( .A(w),       .B(e),       .C(t)       );
+Orr  moduleOR2 ( .A(t),       .B(r),       .C(carry_o) );
 endmodule
+
 
 // --- 4-битный полный сумматор ---
 module fulladder4(
@@ -32,8 +41,8 @@ module fulladder4(
   output logic [3:0] sum_o,
   output logic       carry_o
 );
-  // Объявляем провода для переносов между битами!
- logic c1,c2,c3;  // <--- ВОТ КАК НАДО БЫЛО, BAKA!
+  
+ logic c1,c2,c3;  
 
   fulladder fa1 (.a_i(a_i[0]), .b_i(b_i[0]), .carry_i(carry_i), .sum_o(sum_o[0]), .carry_o(c1));
   fulladder fa2 (.a_i(a_i[1]), .b_i(b_i[1]), .carry_i(c1),      .sum_o(sum_o[1]), .carry_o(c2));
@@ -41,7 +50,7 @@ module fulladder4(
   fulladder fa4 (.a_i(a_i[3]), .b_i(b_i[3]), .carry_i(c3),      .sum_o(sum_o[3]), .carry_o(carry_o));
 endmodule
 
-// --- 32-битный полный сумматор (Твой модуль для теста) ---
+//  32-битный полный сумматор
 module fulladder32(
   input  logic [31:0] a_i, b_i,
   input  logic        carry_i,
@@ -57,4 +66,5 @@ module fulladder32(
   fulladder4 f6 (.a_i(a_i[23:20]), .b_i(b_i[23:20]), .carry_i(c[4]),    .sum_o(sum_o[23:20]), .carry_o(c[5]));
   fulladder4 f7 (.a_i(a_i[27:24]), .b_i(b_i[27:24]), .carry_i(c[5]),    .sum_o(sum_o[27:24]), .carry_o(c[6]));
   fulladder4 f8 (.a_i(a_i[31:28]), .b_i(b_i[31:28]), .carry_i(c[6]),    .sum_o(sum_o[31:28]), .carry_o(carry_o));
+
 endmodule
